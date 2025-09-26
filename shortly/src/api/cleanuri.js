2d35cc1cd8a61a6f194fe86a7f7948f40e5d6ad7
Ifeoluwa <iamsamnice@gmail.com>
2025-09-26 12:00:49 +0100

    Removed Console Messages

--- shortly/src/api/cleanuri.js
+++ shortly/src/api/cleanuri.js
@@ -1,19 +1,3 @@
-// import axios from "axios";
-
-// export async function shortenLink(URI) {
-//   const url = "/api/v1/shorten";
-//   const header = { "Content-Type": "application/x-www-form-urlencoded" };
-//   const body = new URLSearchParams({ url: URI });
-
-//   try {
-//     const response = await axios.post(url, body, { header });
-//     return response.data;
-//   } catch (error) {
-//     console.log(error);
-//     return { error, message: "Internal Server error" };
-//   }
-// }
-
 import axios from "axios";
 
 export async function shortenLink(URI) {
